ad47c2edf60677533db264beaf95022ec2b7db61
victor <2585414795@qq.com>
2019-02-17 09:57:05 +0800

    setting up configurations

---
+++ config.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+# basic config settings here
+
+import os
+
+
+class config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(16)
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_DEFAULT_SENDER = os.environ.get('MAILD_DEFAULT_SENDER')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_SSL = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class devconfig(config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.getcwd() + '/dev.db'
+
+
+class testconfig(config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.getcwd() + '/test.db'
+
+
+class production_config(config):
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(os.environ.get('DB_USER'),
+                                                                      os.environ.get('DB_PASS'),
+                                                                      os.environ.get('DB_HOST'),
+                                                                      os.environ.get('DB_PORT'),
+                                                                      os.environ.get('DB_TABLE'))
+
+
+config = {
+    'development': devconfig,
+    'test': testconfig,
+    'production_config': production_config,
+    'default': devconfig
+}
