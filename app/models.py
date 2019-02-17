646ac2c9ab96b67596a0185c8da8b7783dd5ea8d
victor <2585414795@qq.com>
2019-02-17 10:06:00 +0800

    create User model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+# encoding:utf-8
+# create db models here
+
+from . import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(128), unique=True, nullable=False, index=True)
+    password_hash = db.Column(db.String(128), nullable=False)
