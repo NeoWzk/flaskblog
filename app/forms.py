8eb578453a00cdea57007c93c41d7b22819b20be
victor <2585414795@qq.com>
2019-02-18 15:55:24 +0800

    add email and username verification functions in form model

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,8 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, EqualTo, Optional, Email, Length
 from wtforms import StringField, PasswordField, SubmitField, RadioField
+from wtforms import ValidationError
+from app.models import User
 
 
 class signupForm(FlaskForm):
@@ -12,6 +14,16 @@ class signupForm(FlaskForm):
     password = PasswordField(validators=[DataRequired(), Length(6, 15, message='Password length between 6-15')])
     confirm = PasswordField(validators=[DataRequired(), EqualTo('password', message='Password not matches')])
     submit = SubmitField()
+
+    # two customized functions for validating usename and email
+    # must be started with 'validate_'
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(message='Username already taken!')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered!')
 
 
 class loginForm(FlaskForm):
--- app/main/views.py
+++ app/main/views.py
@@ -18,15 +18,22 @@ def index():
 def register():
     form = signupForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and user.email == form.email.data:
-            flash('Username and email already taken!')
-            return redirect(url_for('auth.sign_in'))
-        else:
-            user = User(username=form.username.data, email=form.email.data, password=form.confirm.data)
-            db.session.add(user)
-            db.session.commit()
-            send_mail('New User Just Joined', '2585414795@qq.com', 'mail/new_user', user=user.username)
-            flash('congrats! You\'re successfully registered')
+        # user = User.query.filter_by(username=form.username.data).first()
+        # if user:
+        #     flash('Username and email already taken!')
+        #     return redirect(url_for('auth.sign_in'))
+        # else:
+        #     user = User(username=form.username.data, email=form.email.data, password=form.confirm.data)
+        #     db.session.add(user)
+        #     db.session.commit()
+        #     send_mail('New User Just Joined', '2585414795@qq.com', 'mail/new_user', user=user.username)
+        #     flash('congrats! You\'re successfully registered')
+        # before adding validate_username and validate_email functions
+        # after adding 'validate_' functions
+        user = User(username=form.username.data, email=form.email.data, password=form.confirm.data)
+        db.session.add(user)
+        db.session.commit()
+        send_mail('New User Just Joined', '2585414795@qq.com', 'mail/new_user', user=user.username)
+        flash('congrats! You\'re successfully registered')
     return render_template('register.html', form=form)
 
