1fb19dded74f4715171a9339666b36add6efedcd
victor <2585414795@qq.com>
2019-02-26 11:05:43 +0800

    usual updates

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@
 # define routes for authenticated url
 
 from flask import render_template, redirect, url_for, flash, request
-from app.forms import signupForm, loginForm
+from app.forms import signupForm, loginForm, profileForm
 from ..models import User
 from flask_login import login_required, login_user, logout_user, current_user
 from ..auth import auth
@@ -51,7 +51,6 @@ def email_confirmation(token):
         return redirect(url_for('main.index'))
 
 
-
 @auth.route('/login', methods=['GET', 'POST'])
 def sign_in():
     form = loginForm()
@@ -64,6 +63,7 @@ def sign_in():
             flash('Invalid credentials')
             return redirect(url_for('auth.sign_in'))
     return render_template('auth/login.html', form=form)
+
 
 @auth.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -78,18 +78,61 @@ def dashboard():
     return render_template('auth/dashboard.html')
 
 
-# @auth.before_app_request
-# def before_request():
-#     if current_user.is_authenticated \
-#             and current_user.confirm is not True\
-#             and request.endpoint[:5] != 'auth.'\
-#             and request.endpoint != 'static':
-#         return redirect(url_for('auth.unconfirmed'))
-#
-#
-# @auth.route('/unconfirmed')
-# def unconfirmed():
-#     return render_template('unconfirmed.html')
+@auth.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    user = User.query.filter_by(email=current_user.email).first()
+    data = {
+        'username': user.username,
+        'email': user.email,
+    }
+    return render_template('auth/account.html', data=data)
 
 
+@auth.route('/account/edit', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = profileForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=current_user.username).first()
+        username = form.username.data
+        email = form.email.data
+        password = form.confirm.data
+        user.username = username
+        user.email = email
+        user.password = password
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Details Changed!')
+        send_mail('Account Details Updated', user.email, 'mail/info_update', user=user.username)
+        return redirect(url_for('auth.account'))
+    return render_template('auth/edit_profile.html', form=form)
 
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and \
+            current_user.confirmed is not True and \
+            request.endpoint[:5] != 'auth.' and \
+            request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    return render_template('unconfirmed.html')
+
+
+@auth.route('/resend_confirm_msg')
+@login_required
+def resend_confirm_msg():
+    if current_user.is_authenticated:
+        token = current_user.generate_confirm_token()
+        send_mail('Email Confirmation Needed',
+                  current_user.email,
+                  'confirm/user_confirm',
+                  user=current_user.username,
+                  token=token)
+        send_mail('Confirmation Msg Resent', '2585414795@qq.com', 'confirm/resend_confirm')
+        flash('Confirmation Message have been resent successfully!')
+    return render_template('unconfirmed.html')
--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,11 @@ class loginForm(FlaskForm):
     email = StringField(validators=[DataRequired(), Email(message='Invalid email add')])
     password = PasswordField(validators=[DataRequired(), Length(6, 15, message='Password length between 6-15')])
     submit = SubmitField()
+
+
+class profileForm(FlaskForm):
+    email = StringField(validators=[DataRequired(), Email(message='Invalid email add')])
+    username = StringField(validators=[DataRequired(), Length(3, 15, message='Username length between 3-15')])
+    password = PasswordField(validators=[DataRequired(), Length(6, 15, message='Password length between 6-15')])
+    confirm = PasswordField(validators=[DataRequired(), EqualTo('password', message='Password not matches')])
+    submit = SubmitField()
